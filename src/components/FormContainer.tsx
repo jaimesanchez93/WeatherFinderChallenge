b3ec6016aabc3eddfd60f9d93960b99a2f3be95e
jaime <jaimesanchez.93@gmail.com>
2020-12-26 16:53:14 +0100

    refactor (form): Handled Errors in getWeather method

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -20,32 +20,36 @@ const FormContainer = () => {
    const [error, setError] = useState<string | null>(null);
 
    const getWeather = async (e: any) => {
-      e.preventDefault();
-      const city = e.target.elements.city.value || "Madrid";
-      const country = e.target.elements.country.value || "es";
-      const api_call = await fetch(
-         `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`
-      );
-      const data = await api_call.json();
-      if (city && country) {
-         setState({
-            temperature: data.main.temp,
-            city: data.name,
-            country: data.sys.country,
-            humidity: data.main.humidity,
-            description: data.weather[0].description,
-         });
-         setError("");
-      } else {
-         setState({
-            temperature: undefined,
-            city: undefined,
-            country: undefined,
-            humidity: undefined,
-            description: undefined,
-         });
-         setError("Please enter the values.")
+      try {
+         e.preventDefault();
+         const city = e.target.elements.city.value || "Madrid";
+         const country = e.target.elements.country.value || "es";
+         const api_call = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`
+         );
+         const data = await api_call.json();
+         if (data.cod === 200) {
+            setState({
+               temperature: data.main.temp,
+               city: data.name,
+               country: data.sys.country,
+               humidity: data.main.humidity,
+               description: data.weather[0].description,
+            });
+         } else {
+            setState({
+               temperature: undefined,
+               city: undefined,
+               country: undefined,
+               humidity: undefined,
+               description: undefined,
+            });
+            throw (city && country) ? data.message : "Please enter the values";
+         }
+      } catch (error) {
+         setError(error);
       }
+
    };
 
    return (
